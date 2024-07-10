97bebc33fc264dd9b0dd756b23601869fadd8c3f
Michael Gorbunov <mg0rby@yandex.ru>
2024-07-10 13:12:09 +0300

    class Article

--- gl_15_2_best_oop/article.py
+++ gl_15_2_best_oop/article.py
@@ -1,32 +1,40 @@
 class Article:
-    """"Knacc Ona XpaHexus cTaTbu """
+    """ Класс для хранения статьи """
     article_id: int
     title: str
     content: str
-    articles = dict()
 
-    def __init__(self, title, content):
-        """ KOHCTPYKTOp Ona cTaTbu """
+    articles = dict()  # атрибут на уровне класса для хранения всех статей
+
+    def __init__(self, title: str, content: str):
+        """ Конструктор для статьи """
         self.article_id = self.get_new_id()
         self.title = title
         self.content = content
 
     def get_new_id(self) -> int:
-        """ Metod dna nonyyexun ID cnedyowyet cTaTbu """
+        """ Метод для получения ID следующей статьи """
         if len(self.articles) > 0:
-            return (max(self.articles)) + 1
+            return max(self.articles.keys()) + 1
         return 1
 
     @classmethod
     def insert(cls, title: str, content: str):
-        """ MeTod dna co3daHUuA U dobaBNeHUA cTaTbU """
+        """ Метод для создания и добавления статьи """
         new_article = cls(title, content)
         cls.articles[new_article.article_id] = new_article
         return new_article
+
+    @classmethod
+    def search(cls, article_id):
+        """ Метод для поиска статьи по ID """
+        # return cls('test3', 'test3')
+        return cls.articles[article_id]
 
 
 if __name__ == '__main__':
     new_article_1 = Article.insert('test1', 'test1')
     print(new_article_1.article_id)
+
     new_article_2 = Article.insert('test2', 'test2')
     print(new_article_2.article_id)
